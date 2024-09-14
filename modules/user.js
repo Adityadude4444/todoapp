30606465b18b04b7aa617afc2ac85dc21f990281
Adityadude4444 <162564995+Adityadude4444@users.noreply.github.com>
2024-09-14 20:55:54 +0530

    Add files via upload

---
+++ modules/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://admin:GlG3TSuhjmhkud2V@cluster0.qo48i.mongodb.net/401"
+);
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  email: String,
+  password: String,
+  todos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "todo",
+    },
+  ],
+});
+
+module.exports = mongoose.model("user", userSchema);
